79b35111a928a59514c21a3a3dda687bece3d931
Oskar Tarka <oskar0208@gmail.com>
2025-10-29 14:07:01 +0100

    fix: removed redundant try catch and AsyncSession is now imported from sqlmodel

--- backend/API/app/core/session.py
+++ backend/API/app/core/session.py
@@ -1,15 +1,16 @@
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlmodel.ext.asyncio.session import AsyncSession
+
 from app.core.config import settings
 
-async_engine = create_async_engine(settings.DB_URL, echo=True, future=True)
-AsyncSessionLocal = async_sessionmaker(bind=async_engine, class_=AsyncSession, expire_on_commit=False)
+async_engine = create_async_engine(settings.DB_URL, echo=True)
+
+AsyncSessionLocal = async_sessionmaker(
+    bind=async_engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+)
 
 async def get_session():
     async with AsyncSessionLocal() as session:
-        try:
-            yield session
-        except Exception:
-            await session.rollback()
-            raise
-        finally:
-            await session.close()
+        yield session
