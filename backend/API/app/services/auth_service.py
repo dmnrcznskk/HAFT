541aacf5682489ebf0ec1e48f6bcc43745d72a65
Oskar Tarka <oskar0208@gmail.com>
2025-11-03 10:55:29 +0100

    refactor: changed password_hash to pwdlib

--- backend/API/app/services/auth_service.py
+++ backend/API/app/services/auth_service.py
@@ -1,7 +1,6 @@
-
-import bcrypt
 from fastapi import HTTPException
 from fastapi.openapi.utils import status_code_ranges
+from pwdlib import PasswordHash
 from starlette import status
 
 from app.models.nn_user import CreateNNUser, NNUser
@@ -10,6 +9,7 @@ from app.models.nn_user import CreateNNUser, NNUser
 class AuthService:
     def __init__(self, repo):
         self.repo = repo
+        self.password_hash = PasswordHash.recommended()
 
     async def register_user(self, new_user:CreateNNUser):
         existing_user = await self.repo.get_user_by_email(new_user.email)
@@ -18,12 +18,7 @@ class AuthService:
                 status_code = status.HTTP_400_BAD_REQUEST,
                 detail = "User with this email already exists"
             )
-
-        byte_password = new_user.password.encode("utf-8")
-        salt = bcrypt.gensalt()
-        hashed_password = bcrypt.hashpw(byte_password, salt)
-        hashed_password_str = hashed_password.decode('utf-8')
-
-        mapped_new_user = NNUser(email=new_user.email, hashed_password=hashed_password_str)
+        hashed_password = self.password_hash.hash(new_user.password)
+        mapped_new_user = NNUser(email=new_user.email, hashed_password=hashed_password)
 
         return await self.repo.create_user(mapped_new_user)
