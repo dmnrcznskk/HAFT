8a9277e5b1a4d521c2ff29a848323b9eed4e3f83
Oskar Tarka <oskar0208@gmail.com>
2025-11-24 11:42:11 +0100

    feat: tokens
    Token endpoint returning access token and setting refresh token. Code cleaning by black

---
+++ backend/API/app/core/security.py
@@ -0,0 +1,30 @@
+from datetime import datetime, timedelta, timezone
+from app.core.config import settings
+import jwt
+
+
+def create_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire, "type": "bearer"})
+    encoded_jwt = jwt.encode(
+        to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM
+    )
+    return encoded_jwt
+
+
+def get_payload(token: str, credentials_exception):
+    try:
+        payload = jwt.decode(
+            token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM]
+        )
+
+        if payload.get("sub") is None:
+            raise credentials_exception
+
+        return payload
+    except jwt.InvalidTokenError:
+        raise credentials_exception
--- backend/API/app/routers/auth_router.py
+++ backend/API/app/routers/auth_router.py
@@ -1,7 +1,10 @@
 from fastapi import APIRouter
 from fastapi.params import Depends
+from fastapi.security import OAuth2PasswordRequestForm
 from starlette import status
+from starlette.responses import Response
 
+from app.core.config import settings
 from app.core.dependencies import get_auth_service
 from app.models.nn_user import CreateNNUser
 from app.services.auth_service import AuthService
@@ -10,6 +13,26 @@ auth_router = APIRouter()
 
 
 @auth_router.post("/register/", status_code=status.HTTP_201_CREATED)
-async def register(new_user: CreateNNUser, auth_service: AuthService = Depends(get_auth_service)):
+async def register(
+    new_user: CreateNNUser, auth_service: AuthService = Depends(get_auth_service)
+):
     await auth_service.register_user(new_user)
     return {"message": "User created successfully"}
+
+
+@auth_router.post("/token/", status_code=status.HTTP_200_OK)
+async def login(
+    response: Response,
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    auth_service: AuthService = Depends(get_auth_service),
+):
+    access_token, refresh_token = await auth_service.login_user(
+        form_data.username, form_data.password
+    )
+    response.set_cookie(
+        key="refresh_token",
+        value=refresh_token.access_token,
+        httponly=True,
+        secure=settings.IS_PRODUCTION,
+    )
+    return access_token
